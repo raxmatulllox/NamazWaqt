97dad470d394c92f69a92f6e157f29330c5a3d2c
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-25 16:46:39 +0500

    Added home functions

--- Waqt/views.py
+++ Waqt/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+from requests import get
+
 
 # Create your views here.
+def home(request):
+    # bs4 scraping
+    url = "http://www.islom.uz"
+    response = get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    element = soup.find('div', class_='in_header_p')
+
+    result = element.text.split()
+    return render(request, 'Waqt/index.html', {'vaqt': result})
