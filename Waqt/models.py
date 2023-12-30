bc4a07471a8dc95986729258cf53391a135a54d4
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-30 20:57:56 +0500

    Add: Added ProfileModel

--- Waqt/models.py
+++ Waqt/models.py
@@ -1,3 +1,17 @@
+from allauth.socialaccount.models import SocialAccount
 from django.db import models
 
-# Create your models here.
+
+# Create your views here.
+class ProfileModel(models.Model):
+        social_account = models.OneToOneField(SocialAccount, on_delete=models.CASCADE)
+        created_at = models.DateTimeField(auto_now_add=True)
+        email = models.EmailField()
+        name = models.CharField(max_length=255)
+        picture = models.URLField(null=True, blank=True)
+
+        class Meta:
+            db_table = 'profilemodel'
+
+        def __str__(self):
+            return str(self.created_at)
