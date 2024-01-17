21226e7d08e532020f4c5250ec9c8518db11a546
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-17 20:44:43 +0500

    add: added AndijonNamazWaqt

--- Waqt/urls.py
+++ Waqt/urls.py
@@ -1,1 +1,9 @@
-from django.urls import pathfrom . import viewsurlpatterns = [    path('', views.NamazTimesViews),    path('', views.home, name='home'),]
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.NamazTimesViews),
+    path('andijon/', views.AndijonNamazWaqt, name='andijon'),
+]
