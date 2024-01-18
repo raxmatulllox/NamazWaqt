25e4079c3a4f469d34f4d6f228e0c98e3086f206
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-18 20:04:53 +0500

    add: city timesviews url

--- Waqt/urls.py
+++ Waqt/urls.py
@@ -5,5 +5,21 @@ from . import views
 
 urlpatterns = [
     path('', views.NamazTimesViews),
+    # path('map/', views.map_view, name='map_view'),
     path('andijon/', views.AndijonNamazWaqt, name='andijon'),
+    path('buxoro/', views.BuxoroNamazWaqt, name='buxoro'),
+    path('denov/', views.DenovNamazWaqt, name='denov'),
+    path('guliston/', views.GulistonNamazWaqt, name='guliston'),
+    path('jizzax/', views.JizzaxNamazWaqt, name='jizzax'),
+    path('margilon/', views.MargilanNamazWaqt, name='margilan'),
+    path('namangan/', views.NamanganNamazWaqt, name='namangan'),
+    path('navoiy/', views.NavoiyNamazWaqt, name='navoiy'),
+    path('nukus/', views.NukusNamazWaqt, name='nukus'),
+    path('olmaliq/', views.OlmaliqNamazWaqt, name='olmaliq'),
+    path('qarshi/', views.QarshiNamazWaqt, name='qarshi'),
+    path('qoqan/', views.QoqanNamazWaqt, name='qoqan'),
+    path('samarqand/', views.SamarqandNamazWaqt, name='samarqand'),
+    path('termiz/', views.TermizNamazWaqt, name='termiz'),
+    path('xiva/', views.XivaNamazWaqt, name='xiva'),
+
 ]
