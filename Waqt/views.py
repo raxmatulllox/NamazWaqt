91473a3731ad6e9f2744f43dbf06018908886d8a
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-02-01 19:26:26 +0500

    added SendNamazWaqtiToEmailViews

--- Waqt/views.py
+++ Waqt/views.py
@@ -1,24 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup
 from requests import get
 from django.conf import settings
+from django.http import HttpResponse
 
+from .models import SendMessageModel, ProfileModel
+from .forms import SendMessageForm
 from . import scripts
 
 
 # Create your views here.
 def NamazTimesViews(request):
     # bs4 scraping
-    url = "http://www.islom.uz/"
-    response = get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    element = soup.find('div', class_='in_header_p')
-    result = element.text.split()
+    try:
+        url = "http://www.islom.uz/"
+        response = get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        element = soup.find('div', class_='in_header_p')
+        result = element.text.split()
+        # print(request.user.email)
+        # print(result)
+        data = {'city': 'Toshkent', 'bomdod': result[5], 'quyosh': result[8], 'peshin': result[11], 'asr': result[14],
+                'shom': result[17], 'hufton': result[20]}
 
-    # print(result)
-    return render(request, 'Waqt/index.html', {'city': 'Toshkent', 'bomdod': result[5],
-                                               'quyosh': result[8], 'peshin': result[11], 'asr': result[14],
-                                               'shom': result[17], 'hufton': result[20]})
+        return render(request, 'Waqt/index.html', data)
+
+    except AttributeError:
+        return render(request, 'Waqt/index.html', context=scripts.toshkent())
+
+
+@login_required
+def SendNamazWaqtiToEmailViews(request):
+    # print(request.user.email)
+    if request.method == 'POST':
+        form = SendMessageForm(request.POST)
+        if form.is_valid():
+            send = form.save(commit=False)
+            send.email = request.user.email
+            send.save()
+            return redirect('home')
+    else:
+        form = SendMessageForm()
+
+    return render(request, 'Waqt/sendform.html', {'form': form})
 
 
 def AndijonNamazWaqt(request):
