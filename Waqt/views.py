7e83af05e4324ed017431c41c49eac4e936d3bb3
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-02-18 17:54:35 +0500

    update views, add some function

--- Waqt/views.py
+++ Waqt/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.conf import settings
+from datetime import timedelta
 from bs4 import BeautifulSoup
 from requests import get
-from django.conf import settings
-from django.http import HttpResponse
 
-from .models import SendMessageModel, ProfileModel
+from .models import SendMessageModel
 from .forms import SendMessageForm
 from . import scripts
 
@@ -22,11 +24,12 @@ def NamazTimesViews(request):
         # print(request.user.email)
         # print(result)
         data = {'city': 'Toshkent', 'bomdod': result[5], 'quyosh': result[8], 'peshin': result[11], 'asr': result[14],
-                'shom': result[17], 'hufton': result[20]}
+                'shom': result[17], 'hufton': result[20], 'vaqt_hijri': scripts.vaqt_hijri(), 'vaqt_mill': scripts.vaqt_mill()}
+        # print(data)
 
-        return render(request, 'Waqt/index.html', data)
+        return render(request, 'Waqt/index.html', context=scripts.toshkent())
 
-    except AttributeError:
+    except:
         return render(request, 'Waqt/index.html', context=scripts.toshkent())
 
 
@@ -36,8 +39,14 @@ def SendNamazWaqtiToEmailViews(request):
     if request.method == 'POST':
         form = SendMessageForm(request.POST)
         if form.is_valid():
+            email = request.user.email
+            existing_send_message = SendMessageModel.objects.filter(email=email).first()
+
+            if existing_send_message:
+                existing_send_message.delete()
+
             send = form.save(commit=False)
-            send.email = request.user.email
+            send.email = email
             send.save()
             return redirect('home')
     else:
@@ -113,3 +122,37 @@ def MapView(request):
 
 def testmap(request):
     return render(request, 'Waqt/test.html')
+
+
+def show_email(request):
+    send_messages = SendMessageModel.objects.all()
+    emails = [send_message.email for send_message in send_messages]
+    print(emails)
+    return render(request, 'Waqt/test.html', {'emails': emails})
+
+
+def send_email_according_to_recurrent(request):
+    objects_to_send_email = SendMessageModel.objects.all()
+    for obj in objects_to_send_email:
+        if obj.recurrent == '1':
+            subject = 'namazwaqti.uz xabarnomasi'
+            message = 'Sizning xabaringiz matni'
+            sender_email = settings.EMAIL_HOST_USER
+            recipient_email = obj.email
+            send_mail(subject, message, sender_email, [recipient_email])
+
+        elif obj.recurrent == '2':
+            current_date = timezone.now()
+            two_days_ago = current_date - timedelta(days=2)
+            if obj.date.date() <= two_days_ago.date():
+                subject = 'namazwaqti.uz xabarnomasi'
+                message = 'Sizning xabaringiz matni!'
+                sender_email = settings.EMAIL_HOST_USER
+                recipient_email = obj.email
+                send_mail(subject, message, sender_email, [recipient_email])
+
+    return render(request, 'Waqt/sendform.html')
+
+
+def week(req):
+    return render(req, 'Waqt/week.html')
